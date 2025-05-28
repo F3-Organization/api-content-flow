066c160110dda9e97ece0ae1c0b2eda98a2282e9
Felipe Oliveira <flipeaz342@gmail.com>
2025-05-27 20:55:33 -0400

    feat: add environment configurations

---
+++ src/config/env.ts
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+export const env = {
+  time_zone: process.env.TZ,
+  client: process.env.DB,
+  connection: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+};

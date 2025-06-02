bc3adce5bf03174011b896654c1046b3ec39b0ad
Felipe Oliveira <flipeaz342@gmail.com>
2025-06-02 06:38:14 -0400

    fix: add expires to generate token

--- src/infra/services/jwt.ts
+++ src/infra/services/jwt.ts
@@ -2,5 +2,8 @@ import jwt from "jsonwebtoken";
 import { env } from "@/config/env";
 
 export function generateToken(userId: string) {
-  return jwt.sign({ userId }, env.secret!, { algorithm: "HS256" });
+  return jwt.sign({ userId }, env.secret!, {
+    algorithm: "HS256",
+    expiresIn: "1d",
+  });
 }

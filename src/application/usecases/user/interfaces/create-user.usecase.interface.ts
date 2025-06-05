82a6ffa1878fb11721c337c399dbc9b34a441561
Felipe Oliveira <flipeaz342@gmail.com>
2025-06-04 22:34:56 -0400

    refactor: remove some properties from ICreateUserNamespace input

--- src/application/usecases/user/interfaces/create-user.usecase.interface.ts
+++ src/application/usecases/user/interfaces/create-user.usecase.interface.ts
@@ -2,16 +2,11 @@ import { AuthProvider, userRoleEnum } from "@/domain/entities";
 
 export namespace ICreateUserNamespace {
   export interface Input {
-    id: string;
     name: string;
     email: string;
-    cpf: string;
-    isActive: boolean;
-    emailVerified: boolean;
     role: userRoleEnum;
     avatar?: string;
     provider: AuthProvider;
     password: string;
-    updatedAt?: Date;
   }
 }

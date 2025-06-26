0930caa7415114d6e17e8dc81bb779925ec53ac6
Felipe Oliveira <flipeaz342@gmail.com>
2025-06-26 11:40:06 -0400

    style: rename subscription stripe data repository

--- src/application/repositories/index.ts
+++ src/application/repositories/index.ts
@@ -2,5 +2,5 @@ export * from "./user-repository.interface";
 export * from "./auth-repository.interface";
 export * from "./plan-repository.interface";
 export * from "./subscription-repository.interface";
-export * from "./subscription-stripe-data-model-repository.interface";
+export * from "./subscription-stripe-data-repository.interface";
 export * from "./recovery-password-repository.interface";
